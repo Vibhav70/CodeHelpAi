c97db840680a7f9c56ae931ce182832bf59dc644
AadityaPandey30 <aprock3004@gmail.com>
2025-10-15 01:55:16 +0530

    Added Tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(),],
   server: {
     proxy: {
       // string shorthand: http://localhost:5173/api -> http://localhost:8000/api
